cda8e4087bdb1b3ff98bd63722ff64de4a0f25c0
Àlex Ollé <alex.olle.93@gmail.com>
2024-04-04 14:58:36 +0200

    Test without useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 import { Header } from "../components/Header/Header";
 import { Loading } from "../components/Loading/Loading";
-import { getMonthData } from "../helpers/apiCalls";
+// import { getMonthData } from "../helpers/apiCalls";
 import { Toggle } from "../components/Toggle/Toggle";
 import { List } from "../components/List/List";
 import { ChangeMonthForm } from "../components/changeMonthForm/ChangeMonthForm";
@@ -15,17 +15,21 @@ export const Home = () => {
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
   const [shownArr, setShownArr] = useState(null);
 
-  const getData = useCallback(async () => {
-    await getMonthData(currentMonth, setMonth);
-  }, [currentMonth]);
+  fetch(process.env.REACT_APP_API_URL + "month/" + currentMonth)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+      setMonth(res);
+    })
+    .then(() => {
+      setIsLoading(false);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   const { name, fruits, vegetables } = month;
-
-  useEffect(() => {
-    getData();
-    setShownArr(fruits);
-    setIsLoading(false);
-  }, [getData, fruits]);
 
   const handleShownArr = (type) => {
     type === "fruits" ? setShownArr(fruits) : setShownArr(vegetables);
@@ -42,7 +46,7 @@ export const Home = () => {
         <main>
           <h2>Fruites i verdures de temporada del mes de {name}</h2>
           <Toggle handleShownArr={handleShownArr} />
-          <List arr={shownArr} />
+          <List arr={shownArr ? shownArr : fruits} />
           <ChangeMonthForm
             handleChange={handleChange}
             currentMonth={currentMonth}
