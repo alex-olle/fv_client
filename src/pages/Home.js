4601a861a649a3db61a4e1544e70e51e48912dfb
Àlex Ollé <alex.olle.93@gmail.com>
2024-04-07 18:20:39 +0200

    Delete unused files. Add logos and favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,10 @@ import { Header } from "../components/Header/Header";
 import { Loading } from "../components/Loading/Loading";
 import { Toggle } from "../components/Toggle/Toggle";
 import { List } from "../components/List/List";
-import { ChangeMonthForm } from "../components/changeMonthForm/ChangeMonthForm";
 
 export const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [month, setMonth] = useState("");
-  const [currentMonth, setCurrentMonth] = useState(null);
   const [shownArr, setShownArr] = useState(null);
 
   const fetchData = async (month) => {
@@ -39,10 +37,6 @@ export const Home = () => {
     type === "fruits" ? setShownArr(fruits) : setShownArr(vegetables);
   };
 
-  const handleChange = (e) => {
-    fetchData(e.target.value);
-  };
-
   return (
     <>
       <Header />
@@ -53,10 +47,6 @@ export const Home = () => {
           <h2>Fruites i verdures de temporada del mes de {name}</h2>
           <Toggle handleShownArr={handleShownArr} />
           <List arr={shownArr ? shownArr : fruits} />
-          <ChangeMonthForm
-            handleChange={handleChange}
-            currentMonth={currentMonth}
-          />
         </main>
       )}
     </>
