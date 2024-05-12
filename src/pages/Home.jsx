925677de1282de82168cfc307bcd76424ce645be
Àlex Ollé <alex.olle.93@gmail.com>
2024-05-12 18:15:52 +0200

    Add selector to change month

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { Loading } from "../components/Loading/Loading";
 import { Toggle } from "../components/Toggle/Toggle";
 import { List } from "../components/List/List";
 import { fetchData } from "../services/fetchData";
+import { ChangeMonthForm } from "../components/changeMonthForm/ChangeMonthForm";
 
 export const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,8 +21,12 @@ export const Home = () => {
 
   const { name, fruits, vegetables } = month;
 
-  const handleShownArr = (type) => {
+  const handleShownArr = (type) =>
     type === "fruits" ? setShownArr(fruits) : setShownArr(vegetables);
+
+  const handleChange = (e) => {
+    setIsLoading(true);
+    fetchData(e.target.value, setMonth, setIsLoading);
   };
 
   return (
@@ -34,6 +39,10 @@ export const Home = () => {
           <h2>Fruites i verdures de temporada del mes de {name}</h2>
           <Toggle handleShownArr={handleShownArr} />
           <List arr={shownArr ? shownArr : fruits} />
+          <ChangeMonthForm
+            handleChange={handleChange}
+            currentMonth={month.id}
+          />
         </main>
       )}
     </>
